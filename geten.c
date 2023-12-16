01224bd2cc10cbbc8061eb25a8fe942c856d990e
ian and bantu <ianwanjama78@gmail.com>
2023-12-16 02:40:05 -0800

    0x16. C - Simple Shell

--- geten.c
+++ geten.c
@@ -1,74 +1,93 @@
 #include "shell.h"
 
 /**
- **_strncpy - copies a string
- *@dest: the destination string to be copied to
- *@src: the source string
- *@n: the amount of characters to be copied
- *Return: the concatenated string
+ * get_environ - returns the string array copy of our environ
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
  */
-char *_strncpy(char *dest, char *src, int n)
+char **get_environ(info_t *info)
 {
-	int i, k;
-	char *s = dest;
-
-	i = 0;
-	while (src[i] != '\0' && i < n - 1)
+	if (!info->environ || info->env_changed)
 	{
-		dest[i] = src[i];
-		i++;
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
 	}
-	if (i < n)
+
+	return (info->environ);
+}
+
+/**
+ * _unsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: 1 on delete, 0 otherwise
+ * @var: the string env var property
+ */
+int _unsetenv(info_t *info, char *var)
+{
+	list_t *node = info->env;
+	size_t i = 0;
+	char *p;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
 	{
-		k = i;
-		while (k < n)
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
 		{
-			dest[k] = '\0';
-			k++;
+			info->env_changed = delete_node_at_index(&(info->env), i);
+			i = 0;
+			node = info->env;
+			continue;
 		}
+		node = node->next;
+		i++;
 	}
-	return (s);
+	return (info->env_changed);
 }
 
 /**
- **_strncat - concatenates two strings
- *@dest: the first string
- *@src: the second string
- *@n: the amount of bytes to be maximally used
- *Return: the concatenated string
+ * _setenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ * @var: the string env var property
+ * @value: the string env var value
+ *  Return: Always 0
  */
-char *_strncat(char *dest, char *src, int n)
+int _setenv(info_t *info, char *var, char *value)
 {
-	int i, k;
-	char *s = dest;
+	char *buf = NULL;
+	list_t *node;
+	char *p;
 
-	i = 0;
-	k = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[k] != '\0' && k < n)
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+	while (node)
 	{
-		dest[i] = src[k];
-		i++;
-		k++;
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
 	}
-	if (k < n)
-		dest[i] = '\0';
-	return (s);
-}
-
-/**
- **_strchr - locates a character in a string
- *@s: the string to be parsed
- *@c: the character to look for
- *Return: (s) a pointer to the memory area s
- */
-char *_strchr(char *s, char c)
-{
-	do {
-		if (*s == c)
-			return (s);
-	} while (*s++ != '\0');
-
-	return (NULL);
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
+	return (0);
 }
