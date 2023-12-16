1cb8e0d668fc3ee5a47256a9b2f72f540f3fb531
ian and bantu <ianwanjama78@gmail.com>
2023-12-16 02:36:34 -0800

    0x16. C - Simple Shell

--- exitts.c
+++ exitts.c
@@ -7,9 +7,9 @@
  *@n: the amount of characters to be copied
  *Return: the concatenated string
  */
- char *_strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int i, k;
+	int i, j;
 	char *s = dest;
 
 	i = 0;
@@ -20,11 +20,11 @@
 	}
 	if (i < n)
 	{
-		k = i;
-		while (k < n)
+		j = i;
+		while (j < n)
 		{
-			dest[k] = '\0';
-			k++;
+			dest[j] = '\0';
+			j++;
 		}
 	}
 	return (s);
@@ -34,36 +34,36 @@
  **_strncat - concatenates two strings
  *@dest: the first string
  *@src: the second string
- *@n: amount of bytes to be maximally used
- *Return: the conc
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
  */
- char *_strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	int i, k;
+	int i, j;
 	char *s = dest;
 
 	i = 0;
-	k = 0;
+	j = 0;
 	while (dest[i] != '\0')
 		i++;
-	while (src[k] != '\0' && k < n)
+	while (src[j] != '\0' && j < n)
 	{
-		dest[k] = src[k];
+		dest[i] = src[j];
 		i++;
-		k++;
+		j++;
 	}
-	if (k < n)
+	if (j < n)
 		dest[i] = '\0';
 	return (s);
 }
 
 /**
- **_strchr - locatess
- *@s: string passed
- *@c: character to look
- *Return: (s)
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
  */
- char *_strchr(char *s, char c)
+char *_strchr(char *s, char c)
 {
 	do {
 		if (*s == c)
